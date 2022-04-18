e5405668ae1db4d95d614b83370cf089a9127411
Jayantsharma177 <103908418+Jayantsharma177@users.noreply.github.com>
2022-04-17 18:52:00 -0600

    Add files via upload

---
+++ Group5_Project/Group5_Project/MainGameClass.cs
@@ -0,0 +1,150 @@
+﻿using System;
+namespace Group5_Project
+{
+    public class MainGameClass
+    {
+        // constants for no of rows and columns
+        const int TOTALROWS = 6, TOTALCOLS = 7;
+
+        //declaring array for gridBox
+        private char[,] gridBox;
+
+        //Constants for blank,players
+        const char BLANK = '#', SYMBOL_PLAYER1 = 'X', SYMBOL_PLAYER2 = 'O';
+
+        //variable to count no of pieces dropped
+
+        //variables for total pieces dropped
+        int totalDroppedPieces;
+
+        //declaring main class for the game
+        public MainGameClass()
+        {
+            //allocate memory to the grid box
+            gridBox = new char[TOTALROWS, TOTALCOLS];
+
+            //set all grid columns to blank
+            for (int y = 0; y < TOTALROWS; y++)
+                for (int x = 0; x < TOTALCOLS; x++)
+                    gridBox[y, x] = BLANK;
+        }
+
+        //method to show the grid on the screen
+        public void ShowGrid()
+        {
+            Console.WriteLine("Connect 4 Game - Developed by :Jashanpreet Kaur Gill\n");
+            Console.WriteLine("Connect 4 Game - Developed by :Jayant Sharma\n");
+            Console.WriteLine("Connect 4 Game - Developed by :Gulya Hasanova\n");
+            Console.WriteLine("-----------------------");
+            for (int y = 0; y < TOTALROWS; y++)
+            {
+                Console.Write("|");
+                for (int x = 0; x < TOTALCOLS; x++)
+                {
+
+                    Console.Write(" " + gridBox[y, x].ToString() + " ");
+
+                }
+                Console.Write("|");
+                Console.Write('\n');
+            }
+            Console.WriteLine("-----------------------");
+            for (int i = 1; i <= TOTALCOLS; i++) Console.Write("  " + i.ToString());
+            Console.WriteLine("\n");
+        }
+
+        // method to display the player played colNo
+        // the player symbol will be displayed in the selected colNo
+        public bool DropDisc(char player, int colNo)
+        {
+            colNo--;
+            //check if column is blank if not return back and display the message
+            if (gridBox[0, colNo] != BLANK)
+                return false;
+
+            //display the symbol in topmost available column in the particular row
+            for (int y = 0; y < TOTALROWS; y++)
+            {
+                if ((y == TOTALROWS - 1) || (gridBox[y + 1, colNo] != BLANK))
+                {
+                    gridBox[y, colNo] = player;
+                    break;
+                }
+            }
+            //increase the no of total dropped pieces
+            totalDroppedPieces++;
+            return true;
+        }
+
+        //method to check whether any player is winner
+        // it will check rows wise, then column wise and then diagonal
+        // if at any place 4 symbols are together it will declare the winner
+        public bool CheckWin(char player)
+        {
+            //Check row wise
+            try
+            {
+                for (int y = 0; y < TOTALROWS; y++)
+                    for (int x = 0; x < 4; x++)
+                        if (gridBox[y, x] == player && gridBox[y, x + 1] == player)
+                            if (gridBox[y, x + 2] == player && gridBox[y, x + 3] == player)
+                                return true;
+            }
+            catch (Exception e1)
+            {
+                Console.WriteLine("Error");
+            }
+            // Check Column Wise
+
+            for (int y = 0; y < 3; y++)
+                for (int x = 0; x < TOTALCOLS; x++)
+                    if (gridBox[y, x] == player && gridBox[y + 1, x] == player)
+                        if (gridBox[y + 2, x] == player && gridBox[y + 3, x] == player)
+                            return true;
+
+            // Check diagonals
+
+            for (int y = 0; y < 3; y++)
+            {
+                for (int x = 0; x < TOTALCOLS; x++)
+                {
+
+                    if (gridBox[y, x] == player)
+                    {
+
+                        // check left diagonals:
+                        try
+                        {
+                            if (gridBox[y + 1, x - 1] == player)
+                            {
+                                if (gridBox[y + 2, x - 2] == player)
+                                    if (gridBox[y + 3, x - 3] == player)
+                                        return true;
+                            }
+                        }
+                        catch (IndexOutOfRangeException) { }
+
+                        // check right side diagonals
+                        try
+                        {
+                            if (gridBox[y + 1, x + 1] == player)
+                            {
+                                if (gridBox[y + 2, x + 2] == player)
+                                    if (gridBox[y + 3, x + 3] == player)
+                                        return true;
+                            }
+                        }
+                        catch (IndexOutOfRangeException) { }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public bool IsGridFull()
+        {
+            return totalDroppedPieces >= TOTALROWS * TOTALCOLS;
+        }
+    }
+}
---
+++ Group5_Project/Group5_Project/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace Group5_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome Message
+            Console.WriteLine("Welcome to Connect 4 Game");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Developed by : Jashanpreet Kaur Gill\n");
+            Console.WriteLine("Developed by : Jayant Sharma\n");
+            Console.WriteLine("Developed by : Gulya Hasanova\n");
+
+            //asking for mode of game
+            int mode = 1;
+            int count = 0;
+            Console.WriteLine("Select the Game Mode\n");
+            do
+            {
+
+                Console.Write("1 for Two Player Mode\n0 for Single Player Mode\n\nEnter your Choice:");
+                mode = Convert.ToInt32(Console.ReadLine());
+
+                if (!(mode == 0 || mode == 1)) Console.Write("Not a Valid Value, Try Again....\n");
+
+            } while (!(mode == 0 || mode == 1));
+            //variable to repeat the loop
+            bool state = true;
+
+            //creating object from Main Game class
+            MainGameClass game = new MainGameClass();
+
+            //setting the first player to play
+            char playerNo = 'X';
+
+            //variable to accept the column number
+            int selectColByPlayer;
+
+
+            bool userInputState;
+
+            //loop till the time there is win or tie
+            while (state)
+            {
+                //clear the console and show grid again
+                System.Console.Clear();
+                game.ShowGrid();
+
+                //loop to accept a number within limit otherwise loop is repeated
+                do
+                {
+                    userInputState = true;
+
+                    //prompting the user to enter value
+                    Console.Write("\nPlayer " + playerNo + " Enter your Column Number:");
+
+                    if (Int32.TryParse(Console.ReadLine(), out selectColByPlayer))
+                    {
+                        if (1 <= selectColByPlayer && selectColByPlayer <= 7)
+                        {
+                            if (game.DropDisc(playerNo, selectColByPlayer))
+                            {
+                                userInputState = false;
+                            }
+                            else
+                            {
+                                System.Console.Clear();
+                                game.ShowGrid();
+                                Console.WriteLine("\nSorry, You cannot player in the Column because it is already full.");
+                            }
+                        }
+                        else
+                        {
+                            System.Console.Clear();
+                            game.ShowGrid();
+                            Console.WriteLine("\nSorry, Allowed is within 1 to 7 values only");
+                        }
+                    }
+                    else
+                    {
+                        System.Console.Clear();
+                        game.ShowGrid();
+                        Console.WriteLine("\nSorry, Enter only numeric value.");
+                    }
+                } while (userInputState);
+
+                //calling the check winner function
+                if (game.CheckWin(playerNo))
+                {
+                    System.Console.Clear();
+                    game.ShowGrid();
+                    Console.Write("\nCongratulations!!!, Player " + playerNo + " is Winner");
+                    Console.WriteLine("\nPress Any Key to Continue....");
+                    state = false;
+                }
+                //checking if the grid if full and no player is winner
+                else if (game.IsGridFull())
+                {
+                    System.Console.Clear();
+                    game.ShowGrid();
+                    Console.WriteLine("\nThere is Tie, No Player wins.");
+                    Console.WriteLine("\nPress Any Key to Continue....");
+                    state = false;
+                }
+                else
+                {
+                    if (mode == 1)
+                    {
+                        playerNo = playerNo == 'X' ? 'O' : 'X';
+                    }
+                    else
+                    {
+                        if (playerNo == 'X')
+                        {
+                            playerNo = 'O';
+                            //calling the check winner function
+
+                            Random random = new Random();
+                            count++;
+                            if (count % 2 == 0 || count % 5 == 0)
+                                selectColByPlayer = random.Next(1, 7);
+
+                            game.DropDisc(playerNo, selectColByPlayer);
+
+                            game.ShowGrid();
+
+                            playerNo = 'X';
+                        }
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
